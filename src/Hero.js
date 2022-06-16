2cbe91b39157d535a49d3d59b9853866bc2e9e80
Moelaal <medo.elaal@gmail.com>
2022-06-16 23:01:57 +0200

    navbar stripe style

---
+++ src/Hero.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import phoneImg from './images/phone.svg';
+import { useGlobalContext } from './context';
+
+const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
+
+  return (
+    <section className="hero" onMouseOver={closeSubmenu}>
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>
+            Payments infrastructure <br />
+            for the internet
+          </h1>
+          <p>
+            startups to Fortune software and APIs to accept payments, send
+            payouts, and manage their businesses online.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
